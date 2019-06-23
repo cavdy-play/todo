a6d0673f98ca16df32cd8857575ab1423cf10e19
cavdy <cavdyofficials@gmail.com>
2019-06-23 12:26:19 +0100

    user features added

--- api/db/config/config.js
+++ api/db/config/config.js
@@ -18,7 +18,10 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres'
   }
 };
